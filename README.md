f57a7143456d400d5fbc9fd7965e83da591ad4cb
Punith1117 <punith11172006@gmail.com>
2024-11-14 18:15:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# web-dev-template
+
+devDependencies:  
+css-loader,
+html-webpack-plugin,
+style-loader,
+webpack,
+webpack-cli,
+webpack-dev-server,
+webpack-merge,
+prettier,
+eslint,
+babel-loader with plugins.
+
+credits:  
+-> reset.css from [joshwcomeau](https://www.joshwcomeau.com/css/custom-css-reset/)
---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+];
